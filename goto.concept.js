859ed435f5b8edf6b823a736df29d7e360f16f83
jardapernik <jaroslav.pernica9@gmail.com>
2020-12-26 05:43:25 +0100

    goto

--- goto.concept.js
+++ goto.concept.js
@@ -1,6 +1,10 @@
+//var fs = require("fs");
+//var stdinBuffer = fs.readFileSync("./sales1M.csv"); // STDIN_FILENO = 0
+//var csv = stdinBuffer.toString();
+
 function csv2arr(str, dlm) {
 	"use strict";
-	dlm = dlm || ',';
+	dlm = (dlm || ',').charCodeAt(0);
 
 	var ROW = 0,
 	    COL = 1,
@@ -8,6 +12,7 @@ function csv2arr(str, dlm) {
 		ESC = 3,
 	    CR  = 4,
 		END = 5,
+		STOP = 6,
 		pushcol  = 0,
 		pushrow  = 1,
 		pushesc  = 2,
@@ -15,7 +20,11 @@ function csv2arr(str, dlm) {
 		startqot = 4,
 		skipchar = 5,
 		pushqot = 6,
-		qot = '"',
+		dft = 0,
+		cqot = '"',
+		qot = cqot.charCodeAt(0),
+		cr = '\r'.charCodeAt(0),
+		lf = '\n'.charCodeAt(0),
 		i = 0,
 		j = 0,
 	    st = ROW,
@@ -23,42 +32,54 @@ function csv2arr(str, dlm) {
 		col = '',
 		res = [],
 		fn,
-		c;
+		c,
+		parstbl = [];
 
-	while ((c = str.charAt(i)) || (st = END)) {
-		switch (st) {
-		case ROW: switch (c) {
-			case qot: st = QOT; fn = startqot; break;
-			case dlm:           fn = pushcol;  break;
-			case '\r':st = CR;  fn = pushrow;  break;
-			default:  st = COL; fn = startcol; }
-			break;
-		case COL: switch (c) {
-			case dlm:  st = ROW; fn = pushcol; break;
-			case '\n': st = ROW; fn = pushrow; break;
-			case '\r': st = CR;  fn = pushrow; break;
-			default:             fn = skipchar; }
-			break;
-		case QOT: switch (c) {
-			case qot: st = ESC; fn = pushqot; break;
-			default:            fn = skipchar; }
-			break;
-		case ESC: switch (c) {
-			case qot:  st = QOT; fn = pushesc; break;
-			case '\n': st = ROW; fn = pushrow;  break;
-			case '\r': st = CR;  fn = pushrow;  break;
-			case dlm:  st = ROW; fn = pushcol;  break;
-			default:   st = COL; fn = startcol;
-			}
-			break;
-		case CR: switch (c) {
-			case '\n': st = ROW; fn = skipchar; break;
-			case '\r':           fn = pushrow;  break;
-			case dlm:  st = ROW; fn = pushcol;  break;
-			case qot:  st = QOT; fn = startqot; break;
-			default:   st = COL; fn = startcol; }
-			break;
-		case END: fn = pushrow; END = 0; }
+	parstbl[ROW] = [];
+	parstbl[COL] = [];
+	parstbl[QOT] = [];
+	parstbl[ESC] = [];
+	parstbl[CR ] = [];
+	parstbl[END] = [];
+
+	var trow = parstbl[ROW],
+		tcol = parstbl[COL],
+		tqot = parstbl[QOT],
+		tesc = parstbl[ESC],
+		tcr  = parstbl[CR],
+		tend = parstbl[END];
+
+	trow[qot] = [QOT, startqot];
+	trow[dlm] = [ROW, pushcol ];
+	trow[cr]  = [CR,  pushrow ];
+	trow[dft] = [COL, startcol];
+
+	tcol[dlm] = [ROW, pushcol ];
+	tcol[lf]  = [ROW, pushrow ];
+	tcol[cr]  = [CR,  pushrow ];
+	tcol[dft] = [COL, skipchar];
+
+	tqot[qot] = [ESC, pushqot ];
+	tqot[dft] = [QOT, skipchar];
+	
+	tesc[qot] = [QOT, pushesc ];
+	tesc[lf]  = [ROW, pushrow ];
+	tesc[cr]  = [CR,  pushrow ];
+	tesc[dlm] = [ROW, pushcol ];
+
+	tcr[lf]   = [ROW, skipchar];
+	tcr[cr]   = [CR,  pushrow ];
+	tcr[dlm]  = [ROW, pushcol ];
+	tcr[qot]  = [QOT, startqot];
+	tcr[dft]  = [COL, startcol];
+
+	tend[dft] = [STOP, pushrow];
+
+	var tmp;
+	while ((c = str.charCodeAt(i)) || (st !== STOP && (st = END))) {
+		tmp = parstbl[st][c] || parstbl[st][dft];
+		st = tmp[0];
+		fn = tmp[1];
 
 		switch (fn) {
 		case pushcol:
@@ -66,16 +87,15 @@ function csv2arr(str, dlm) {
 			row.push(col || str.slice(j, i));
 			j = ++i;
 			col = '';
-			if (fn === pushcol)
-				break;
+			if (fn === pushcol) break;
 			res.push(row);
 			row = [];
 			break;
 		case pushesc:
-			col += qot;
+			col += cqot;
 		case startqot:
 			j = ++i;
-			i = str.indexOf(qot, i);
+			i = str.indexOf(cqot, i);
 			if (i < 0)
 				i = str.length;
 			break;
@@ -92,6 +112,7 @@ function csv2arr(str, dlm) {
 	return res;
 }
 
-for (var i = 0; i < 100000; i++)
-	csv2arr('0,2,3\r\n4,5,"ahoj ""a"" a"');
+var tsv='1\t2\t\t\n"ahoj ""jaa"""\t4\t5\n';
+var csv='1,2,3\n5,6,7';
+console.log(csv2arr(csv));
 
